30c6e74114a52c5463e705e6985963ef9148389c
Grégoire Ménard <108455082+greg-bioinfo@users.noreply.github.com>
2022-11-13 14:48:02 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,45 @@
-#include<iostream>
+ #include<iostream>
 #include "radio.h"
+#include <fstream>
 using namespace std;
 
+std::ofstream flux{"radio.txt",fstream::app};
+
 int main(){
-    string mdp,texte,mdp2;
-    cout<<"entrez mot de passe"<<endl;
-    cin>>mdp;
-    cout<<"entrez texte: "<<endl;
+
+    string nom,prenom,naissance,date,texte,mot_de_passe,type,e,mdp;
+    int num;
+    vector<Cliche> liste_cliche;
+
+    Cliche c(3,"first");
+    Cliche c1(32,"second");
+    liste_cliche.push_back(c);
+    liste_cliche.push_back(c1);
+    cout<<"num: "<<endl;
+    cin>>num;
+    cout<<"type"<<endl;
+    cin>>type;
+    cout<<"etat: "<<endl;
+    cin>>e;
+    cout<<"entrez nom"<<endl;
+    cin>>nom;
+    cout<<"entrez prenom: "<<endl;
+    cin>>prenom;
+    cout<<"entrez nais: "<<endl;
+    cin>>naissance;
+    cout<<"entrez date: "<<endl;
+    cin>>date;
+    cout<<"entrez txt "<<endl;
     cin>>texte;
+    cout<<"entrez mdp "<<endl;
+    cin>>mot_de_passe;
 
-    Compte_rendu cpt(mdp,texte);
-    cout<<"entrez le mdp pour voir le text"<<endl;
-    cin>>mdp2;
-    cpt.Consulter(mdp2);
 
+
+    Radiographie r(num,type,date,e,liste_cliche,nom,prenom,naissance,texte,mot_de_passe);
+    
+    r.get_liste_cliche();
+
+    Liste_radiographie data("radio.json");
 
 }
